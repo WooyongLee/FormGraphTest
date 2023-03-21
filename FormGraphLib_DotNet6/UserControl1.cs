88e17dfad34186fa20d7bc0610713c8101729001
wooyo <wooyong.lee@dabinsystems.com>
2023-03-21 10:15:29 +0900

    1. .NET Project로 생성 및 Winform Usercontrol 가져와서 쓰는 거 확인

---
+++ FormGraphLib_DotNet6/UserControl1.cs
@@ -0,0 +1,10 @@
+﻿namespace FormGraphLib_DotNet6
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
